159ea11b39124126e90b1e49dbd3f43402db9413
Stefan Wong <stfnwong@gmail.com>
2020-12-24 16:49:10 +0000

    Update

--- init.el
+++ init.el
@@ -1,8 +1,61 @@
+;; Init file
+
+;; Colorscheme
 (load-theme 'tango-dark)
+;; Line numbers
+(when (version<= "26.0.50" emacs-version)
+  (global-display-line-numbers-mode))
 
+;; Change default window size
+(if (display-graphic-p)
+    (progn
+      (setq initial-frame-alist
+	    '(
+	      (tool-bar-lines . 0)
+	      (width . 106)   ;; chars
+	      (height . 60)   ;; lines
+	      (left . 50)
+	      (top . 50)))
+      (setq default-frame-alist
+	    '(
+	      (tool-bar-lines . 0)
+	      (width . 105)
+	      (height . 60)
+	      (left . 50)
+	      (top . 50))))
+  (progn
+    (setq initial-frame-alist '( (tool-bar-lines . 0)))
+    (setq default-frame-alist '( (tool-bar-lines . 0)))))
+
+
+;; Packages
 (require 'package)
-
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-
 (setq package-enable-at-startup nil)
 (package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil-visual-mark-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Operate in evil mode by default
+(require 'evil)
+(evil-mode t)
+
+;; Enable Org mode
+(require 'org)
+;; Set org mode to work with files ending in *.org
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+
+;; Add the org-mode bindings
+(global-set-key "\C-c a" 'org-agenda)
+(global-set-key "\C-c l" 'org-store-link)
+(global-set-key "\C-c c" 'org-capture)
